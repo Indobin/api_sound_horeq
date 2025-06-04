f124cb91f0f3c8db2d6254e887e5dbb083b7010e
Satya788. <fegotero7@gmail.com>
2025-06-04 10:16:51 +0700

    .

--- api/server.py
+++ api/server.py
@@ -1,9 +1,1 @@
 from app.main import app
-from mangum import Mangum
-
-if not callable(app):
-    print(f"❌ ERROR: `app` is not a callable object: {type(app)}")
-else:
-    print(f"✅ app is of type: {type(app)}")
-
-handler = Mangum(app)
